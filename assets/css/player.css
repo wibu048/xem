5793369f359152f057edad9b7a1e0b575ba54355
wibu048 <62291140+wibu048@users.noreply.github.com>
2020-04-06 21:11:59 +0700

    Add files via upload

---
+++ assets/css/player.css
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>XtGem.com - Not Found</title>
+    </head>
+    <style>a{color:#1b5a1f;text-decoration:none;font-family:Arial;font-weight:700;}a:hover{text-decoration:underline;}div{margin-top: 10px;}img{border:0;margin-top:10px}</style>
+    <body style="background: #F3FFF3">
+        <div style="text-align:center">
+            <div><div style="display:none"><script type="text/javascript">
+var _qevents = _qevents || [];
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+_qevents.push({
+qacct:"p-0cfM8Oh7M9bVQ"
+});
+</script>
+<noscript>
+<img src="//pixel.quantserve.com/pixel/p-0cfM8Oh7M9bVQ.gif" border="0" height="1" width="1" alt=""/>
+</noscript></div></div>
+	    <div>The page you requested could not be found. It might have been moved or deleted</div>
+            <div><a href="http://xtgem.com"><img src="http://xtgem.com/images/powered.gif" alt="Powered by XtGem.Com" /></a></div>
+        </div>
+    </body>
+</html>
